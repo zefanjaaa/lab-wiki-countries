ff032d65b3d0d50d3dced6f0e32fe573e0726600
Zefanja Timorason <z.timorason@gmail.com>
2023-03-02 17:40:02 +0100

    got iteration 2.4 sort of working

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,45 +1,53 @@
-import countryData from './../countries.json'
-import { useParams, Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import countryData from './../countries.json';
+import { useParams, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
-function CountryDetails(props) {
-    const [foundCountry, setFoundCountry] = useState(null)
-    const [complicated, setComplicated ] = useState(countryData)
+function CountryDetails(land) {
+  const [foundCountry, setFoundCountry] = useState(null);
+  const [complicated, setComplicated] = useState(countryData);
 
-    const { countryId } = useParams()
-    
-    useEffect(() => {
-        const country = countryData.find((oneCountry) => {
-            return oneCountry.alpha3Code === countryId
-        })
+  const { countryId } = useParams();
 
-        if (country) {
-            setFoundCountry(country)
-        }
-    }, [countryId])
-    {
-        complicated.map(value => {
-            return( value.border.includes(value.alpha3Code))
-        })
-        
-}}
-    return (
-        <div>
-            <h1> Country Details</h1>
-            {!foundCountry &&  <Link to='/'><p>How do you spell experience?</p></Link> }
-            {foundCountry && (
-                <>
-                    <h2>{foundCountry.name.common}</h2>
-                    <p>Capital:{foundCountry.capital}</p>
-                    
-                    <p>Borders:{foundCountry.borders}</p>
-                    
-                    <p>Area:{foundCountry.area}</p>
-                    <Link to='/'>GO BACK</Link>
-                </>
-            )}
-        </div>
-    )
+  useEffect(() => {
+    const country = countryData.find((oneCountry) => {
+      return oneCountry.alpha3Code === countryId;
+    });
+
+    if (country) {
+      setFoundCountry(country);
+    }
+  }, [countryId]);
+
+
+  return (
+    <div>
+          <h1> Country Details</h1>
+          
+      {!foundCountry && (
+        <Link to="/">
+          <p>How do you spell experience?</p>
+        </Link>
+      )}
+      {foundCountry && (
+        <>
+          <h2>{foundCountry.name.common}</h2>
+          <p>Capital:{foundCountry.capital}</p>
+          <p>Area:{foundCountry.area}</p>
+          {/* <p>Borders:{foundCountry.borders}</p> */}
+
+         
+                  <p>Border</p>
+                  {foundCountry.borders.map((yy, xx) => {
+                   
+                      const link = `/countries/${yy}`
+                      return <li key={xx}><Link to={link}> {yy}  </Link></li> 
+                  })}
+
+          <Link to="/">GO BACK</Link>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default CountryDetails
+export default CountryDetails;
